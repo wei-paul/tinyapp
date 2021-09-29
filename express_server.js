0a05980450de0276ed54e0d762f4e862064928d3
wei-paul <paul.wei607@gmail.com>
2021-09-29 22:58:12 +0000

    added registration page

--- express_server.js
+++ express_server.js
@@ -14,21 +14,45 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+//find user by email function
+const findUserByEmail = (email) => {
+  for(const userId in users) {
+    const user = users[userId];
+    if (user.email === email) {
+      return user
+    }
+  }
+  return null
+}
+
 app.get("/urls/new", (req, res) => {
-  const templateVars = { username: req.cookies.username };
+  const templateVars = { user: users[req.cookies.user_id] };
   res.render("urls_new", templateVars);
 })
 
 app.get("/urls", (req, res) => {
   console.log(req.cookies["username"])
     const templateVars = {
-    username: req.cookies.username,
+    user: users[req.cookies.user_id],
     urls: urlDatabase
   };
   res.render("urls_index", templateVars);
 })
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies.username };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[req.cookies.user_id] };
   res.render("urls_show", templateVars);
 });
 
@@ -37,6 +61,14 @@ app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[shortURL]
   res.redirect(longURL);
 });
+
+app.get("/register", (req, res) => {
+  const templateVars = {
+    user: users[req.cookies.user_id]
+  }  
+  res.render('registration', templateVars)
+})
+
 app.post("/logout", (req, res) => {
   res.clearCookie("username")
   res.redirect('/urls');
@@ -46,6 +78,33 @@ app.post("/login", (req, res) => {
   res.cookie("username", req.body.username)
   res.redirect('/urls');
 })
+
+app.post("/register", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  // we want make sure that email and password are filled
+  if( !email || !password ) {
+    return res.status(400).send("Email or Password cannot be blank!");
+  }
+
+  // check to see if email exists in database already
+  const user = findUserByEmail(email);
+
+  if(user) {
+    return res.status(400).send('User with that email currently exists!')
+  }
+
+  const id = generateRandomString();
+
+  users[id] = {
+    id: id,
+    email: email,
+    password: password
+  }
+  res.cookie('user_id', id);
+  res.redirect('/urls')
+})
+
 
 app.post("/urls", (req, res) => {
   console.log(req.body); 
